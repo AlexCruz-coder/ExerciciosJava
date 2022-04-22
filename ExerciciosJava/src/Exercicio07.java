da191ef870fae80b3e3f3054b2d577ce21929db1
Alex Cruz <alexbenicio@hotmail.com.br>
2022-04-22 13:45:28 -0300

    Ajustes nos arquivos de exercicios

--- ExerciciosJava/src/Exercicio07.java
+++ ExerciciosJava/src/Exercicio07.java
@@ -3,12 +3,6 @@ public class Exercicio07 {
 
 	public static void main(String args[]){
 		
-		//2.	 Imprima	a	soma	de	1	até	1000.
-		
-		for(Integer i = 1 ; i <= 1000 ; i++) {
-			Integer a = i + 1;
-			System.out.println("A soma de " + i + " + " + a + " é igual a: " + (a+i));
-			
-		}
+
 	}
 }
