1fca150759bedbb58f7bcd9b9450c3b49e259c8b
Alex Cruz <alexbenicio@hotmail.com.br>
2022-05-12 12:00:13 -0300

    Exercicio 5 - em andamento - Elevadores

--- ExerciciosJava/src/Exercicio05.java
+++ ExerciciosJava/src/Exercicio05.java
@@ -16,6 +16,130 @@ dentro dele);
 Sobe : para subir um andar (não deve subir se já estiver no último andar);
 Desce : para descer um andar (não deve descer se já estiver no térreo);
 Encapsular todos os atributos da classe (criar os métodos set e get).*/
+		
+		//Run
+		Elevador e1 = new Elevador();
+		e1.Inicializa(5);
+		e1.Entra();
+		
 			
 	}
+}
+
+
+class Elevador {
+	
+	Integer andar;
+	Integer capacidadeMaxima;
+	Integer quantidadePessoas;
+	
+	//Constructors
+	public Elevador(Integer capacidadeMaxima) {
+		super();
+		this.andar = 0;
+		this.capacidadeMaxima = capacidadeMaxima;
+		this.quantidadePessoas = 0;
+	}
+	
+	public Elevador() {
+	}
+	
+	//Getters e Setters
+	public Integer getAndar() {
+		return andar;
+	}
+	public void setAndar(Integer andar) {
+		this.andar = andar;
+	}
+	public Integer getCapacidadeMaxima() {
+		return capacidadeMaxima;
+	}
+	public void setCapacidadeMaxima(Integer capacidadeMaxima) {
+		this.capacidadeMaxima = capacidadeMaxima;
+	}
+	public Integer getQuantidadePessoas() {
+		return quantidadePessoas;
+	}
+	public void setQuantidadePessoas(Integer quantidadePessoas) {
+		this.quantidadePessoas = quantidadePessoas;
+	}
+	
+	//Métodos
+	public void Inicializa(Integer capacidade) {
+		this.andar = 0;
+		this.capacidadeMaxima = capacidade;
+		this.quantidadePessoas = 0;
+		
+		System.out.println("Instalado novo elevador no prédio com capacidade para transportar "  + this.capacidadeMaxima + " pessoas.");
+	}
+	
+	
+	/*
+	 * 
+	 * Entra : para acrescentar uma pessoa no elevador (só deve acrescentar se ainda houver
+espaço);
+Sai : para remover uma pessoa do elevador (só deve remover se houver alguém
+dentro dele);
+Sobe : para subir um andar (não deve subir se já estiver no último andar);
+Desce : para descer um andar (não deve descer se já estiver no térreo);
+Encapsular todos os atributos da classe (criar os métodos set e get).*/
+	 
+	public void Entra() {
+		if (this.capacidadeMaxima>this.quantidadePessoas) {
+			this.quantidadePessoas++;
+			if (this.quantidadePessoas>1) {
+				System.out.println("Entrou mais uma pessoa no Elevador, que agora transporta " + this.quantidadePessoas + " pessoas.");
+			} else {
+				String andarVerificado = verificarAndar(this.andar);
+				System.out.println("Uma pessoa entrou no elevador que está no " + andarVerificado);
+			}
+			
+		}
+	}
+
+	private String verificarAndar(Integer andarVerificar) {
+		String andarVerificado ="";
+		switch(andarVerificar) {
+			case 0:
+				andarVerificado= "térreo";
+			break;
+			default:
+			String uni[] = {"", "primeiro", "segundo", "terceiro", "quarto", "quinto", "sexto", 
+					"sétimo", "oitavo", "nono"};
+			
+			String dec[] = {"", "décimo", "vigésimo", "trigésimo", "quadragésimo", "quinquagésimo", "sexagésimo", 
+					"septuagésimo", "octagésimo", "nanagésimo"};
+			
+			Integer unidade = andarVerificar%10;
+			Integer dezena = (andarVerificar/10)%10;
+			if(andarVerificar>9) {
+				andarVerificado = dec[dezena] + " " + uni[unidade] + " andar";
+			} else if (andarVerificar<10) {
+				andarVerificado= uni[unidade]+ " andar";
+			} else {
+				andarVerificado= "indefinido";
+			}	 
+		}//fim do Switch
+		return andarVerificado;
+	}
+	
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
